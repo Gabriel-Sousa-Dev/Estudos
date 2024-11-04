e9e69241f16deeb5de4ac1a6b8a177dcb5e6d469
Gabriel-Sousa-Dev <gabrieldesousa.dev@gmail.com>
2024-11-04 17:03:24 -0300

    Adicionei o Position a class

--- typescript/window.ts
+++ typescript/window.ts
@@ -1,32 +1,50 @@
+interface Position{
+    x: number,
+    y: number
+}
+
 class Janela{
     private status: 'minimizado' | 'aberto' | 'fechado'
 
+    private position: Position = {
+        x: 0,
+        y: 0
+    }
+
     constructor(){
-        this.status = 'aberto'
-    }
-
-    fecharJanela(){
-        this.status = 'fechado';
-    }
-
-    abrirJanela(){
         this.status = 'aberto';
     }
 
-    minimizarJanela(){
+    fechar(){
+        this.status = 'fechado';
+    }
+
+    abrir(){
+        this.status = 'aberto';
+    }
+
+    minimizar(){
         this.status = 'minimizado'
     }
 
-    imprimirStatusDaJanela(){
+    imprimirStatus(){
         console.log(this.status);
-        
     }
+
+    definirPosition(newPosition: Position){
+        this.position = newPosition;
+    }
+
+    imprimirPosition(){
+        console.log(JSON.stringify(this.position, null, 2))
+    }
+
 }
 
 let jan = new Janela();
 
-jan.imprimirStatusDaJanela()
+jan.imprimirPosition();
 
-jan.minimizarJanela();
+jan.definirPosition({x: 45, y: 60});
 
-jan.imprimirStatusDaJanela()
+jan.imprimirPosition();
